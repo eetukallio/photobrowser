3000e14de1c7f2be1462f4e2c311985f2fb334ad
Eetu Kallio <eetu.kallio@gmail.com>
2019-11-26 19:12:56 +0200

    Fix more minor component styling issues

--- src/components/PhotoList/PhotoListItem.js
+++ src/components/PhotoList/PhotoListItem.js
@@ -1,38 +1,43 @@
-import React from 'react'
-import { useDispatch } from 'react-redux';
-import { View, Text, Image, StyleSheet, TouchableOpacity, Dimensions } from 'react-native'
-import NavigationService from '../../navigation/NavigationService'
-import { setSelectedPhoto } from '../../slices/selectedPhotoSlice';
+import React from 'react';
+import {useDispatch} from 'react-redux';
+import {
+  View,
+  Text,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+} from 'react-native';
+import NavigationService from '../../navigation/NavigationService';
+import {setSelectedPhoto} from '../../slices/selectedPhotoSlice';
 
 const imageWidth = Dimensions.get('window').width / 3 - 10;
 const imageHeight = imageWidth;
 
 const PhotoListItem = props => {
+  const dispatch = useDispatch();
 
-    const dispatch = useDispatch();
+  const photo = props.photo;
 
-    const photo = props.photo;
+  const handlePress = () => {
+    dispatch(setSelectedPhoto(photo));
+    NavigationService.navigate('Details');
+  };
 
-    const handlePress = () => {
-        dispatch(setSelectedPhoto(photo))
-        NavigationService.navigate('Details')
-    }
-
-    return (
-        <TouchableOpacity onPress={handlePress}  style={styles.container}>
-            <Image style={styles.image} source={{uri: photo.thumbnailUrl}} /> 
-        </TouchableOpacity>
-    )
-}
+  return (
+    <TouchableOpacity onPress={handlePress} style={styles.container}>
+      <Image style={styles.image} source={{uri: photo.thumbnailUrl}} />
+    </TouchableOpacity>
+  );
+};
 
 const styles = StyleSheet.create({
-    image: {
-        height: imageHeight,
-        width: imageWidth,
-        margin: 5,
-        borderRadius: 15
-    }
-})
+  image: {
+    height: imageHeight,
+    width: imageWidth,
+    margin: 5,
+    borderRadius: 15,
+  },
+});
 
-
-export default PhotoListItem
+export default PhotoListItem;
